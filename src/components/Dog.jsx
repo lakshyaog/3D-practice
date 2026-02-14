87f4d9c5f2142823ebca75c338fba8b4781111fc
Lakshya Solanki <lakshyasolanki904@gmail.com>
2026-02-14 22:26:10 +0530

    practice day 1

---
+++ src/components/Dog.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Canvas, useThree} from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+const Dog = () => {
+
+
+    useThree(({camera, scene, gl}) => {
+        console.log(camera.position)
+    })
+
+
+    return (
+        <>   
+
+
+        <OrbitControls />
+        
+        
+        </>
+
+    
+  )
+}
+
+export default Dog
