ae047cf35302212d529508e763b13054bd14bdac
Lakshya Solanki <lakshyasolanki904@gmail.com>
2026-02-24 23:57:15 +0530

    minor psuh

--- src/components/BmwModel.jsx
+++ src/components/BmwModel.jsx
@@ -1,11 +1,27 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 import { useGLTF } from '@react-three/drei'
 
+/**
+ * BmwModel - Renders the BMW M4 Competition 3D model
+ * @param {object} props - Standard Three.js mesh props (position, scale, rotation, etc.)
+ */
 const BmwModel = (props) => {
     const { scene } = useGLTF('/bmw_m4.glb')
+    const modelRef = useRef()
+
+    // Enable shadow casting on all meshes
+    useEffect(() => {
+        scene.traverse((child) => {
+            if (child.isMesh) {
+                child.castShadow = true
+                child.receiveShadow = true
+            }
+        })
+    }, [scene])
 
     return (
         <primitive
+            ref={modelRef}
             object={scene}
             {...props}
         />
